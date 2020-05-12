d1707b9525e4b7cc6d2fa7fda20b0835c7a3b1bf
Doug Metz <dwmetz@gmail.com>
2020-05-12 00:19:02 -0400

    Add files via upload

---
+++ AxCollect.ps1
@@ -0,0 +1,13 @@
+# This script enables investigator access to a users O365 mailbox.
+# dwmetz@gmail.com   https://dwmetz.github.io/ @dwmetz
+# Once enabled sign in using investigator credentials to Axiom
+
+
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+$script:axiom_o365 = Read-Host -Prompt 'Enter the used ID of the target (ex.R12345)'
+$script:axiom_examiner =Read-Host -Prompt 'Enter the used ID of the examiner (ex.R34567)'
+Add-MailboxPermission -Identity "$script:axiom_o365" -User "$script:axiom_examiner" -AccessRights FullAccess
+Write-Host -ForegroundColor Green $script:axiom_examiner 'permissions added to' $script:axiom_o365 'mailbox.'
